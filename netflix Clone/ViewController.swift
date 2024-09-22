2a004a10203815b451ac030d329a1adb5559406c
alam30adeeb <108567018+alam30adeeb@users.noreply.github.com>
2024-09-22 23:27:15 +0530

    Initial Commit

---
+++ netflix Clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  netflix Clone
+//
+//  Created by Adeeb Alam on 22/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ netflix CloneTests/netflix_CloneTests.swift
@@ -0,0 +1,17 @@
+//
+//  netflix_CloneTests.swift
+//  netflix CloneTests
+//
+//  Created by Adeeb Alam on 22/09/24.
+//
+
+import Testing
+@testable import netflix_Clone
+
+struct netflix_CloneTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
